b6665d2b169fb53af67110937c2a863837192afd
pookleblinky <pookleblinky@gmail.com>
2017-05-24 07:49:43 -0400

    Added lein user profile

---
+++ profiles.clj
@@ -0,0 +1,8 @@
+{:user  {:plugins  [[cider/cider-nrepl  "0.14.0"]
+                    [jonase/eastwood  "0.2.4"]
+                    [lein-kibit  "0.1.5"]
+                    [lein-bikeshed  "0.4.1"]
+                    [lein-eftest  "0.3.1"]
+                    [lein-midje  "3.1.3"]
+                    [bolth  "0.1.0"]
+                    [com.jakemccrary/lein-test-refresh  "0.20.0"]]  }}
